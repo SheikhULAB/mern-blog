063310feea5f10b9f2868f22114820bd8d795694
SheikhULAB <tosibalaptop86@gmail.com>
2024-06-04 05:54:25 +0600

    add admin funtionality

--- api/index.js
+++ api/index.js
@@ -20,8 +20,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3002, () => {
-    console.log("server listening on port 3002!!");
+app.listen(3000, () => {
+    console.log("server listening on port 3000!");
 })
 
 
