bb38364f14ee86be51c9640fd148b316262d16d2
SheikhULAB <tosibalaptop86@gmail.com>
2024-06-04 16:19:27 +0600

    dashboard users post show , crud functionality to users and post

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
+// import DashComments from '../components/DashComments';
+// import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -14,13 +18,21 @@ export default function Dashboard() {
     }
   }, [location.search]);
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
+    <div className='min-h-screen flex flex-col md:flex-row'>
       <div className='md:w-56'>
         {/* Sidebar */}
         <DashSidebar />
       </div>
       {/* profile... */}
       {tab === 'profile' && <DashProfile />}
+      {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
+      {/* comments  */}
+      {/* {tab === 'comments' && <DashComments />} */}
+      {/* dashboard comp */}
+      {/* {tab === 'dash' && <DashboardComp />} */}
     </div>
   );
 }
